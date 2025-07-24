cbdac3afe309aa9ce3d2b19d5d25412f9abce186
pram-zk <zakipramudia03@gmail.com>
2025-07-24 10:39:09 +0700

    update terbaru

--- app/src/main/java/com/example/app1zaki/MainActivity.kt
+++ app/src/main/java/com/example/app1zaki/MainActivity.kt
@@ -3,37 +3,39 @@ package com.example.app1zaki
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material3.Button
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.app1zaki.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.btnInstagram.setOnClickListener{
-            val instagramIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/pram.zk/"))
-            startActivity(instagramIntent)
-        }
-
-        binding.btnwa.setOnClickListener{
-            val Nohp = "62895426693500"
-            val waIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://$Nohp"))
-            startActivity(waIntent)
-        }
-
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val btnig = findViewById<Button>(R.id.btnInstagram)
+        btnig.setOnClickListener {
+            val instagramintent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/pram.zk/"))
+            startActivity(instagramintent)
+        }
+
+        val btnwa = findViewById<Button>(R.id.btnwa)
+        btnwa.setOnClickListener {
+            val phone = "62895426693500"
+            val waintent = Intent(Intent.ACTION_VIEW, Uri.parse("https://wa.me/$phone"))
+            startActivity(waintent)
+        }
+
+
     }
 }
